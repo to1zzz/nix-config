2343be0b1d5cf867d983c786883b77c15198ba36
To1z <to1zzz@proton.me>
2026-09-25 20:22:29 +0000

    Add disko.nix configuration for disk setup

---
+++ disko.nix
@@ -0,0 +1,33 @@
+{
+  disko.devices = {
+    disk = {
+      my-disk = {
+        device = "/dev/sdb";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              type = "EF00";
+              size = "256M";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
